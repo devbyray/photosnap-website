3bb345a5115bf546965c7ef330cc0703744bd14c
DevByRayRay <raymon@byrayray.dev>
2023-10-24 21:25:37 +0200

    Added big card to stories

---
+++ components/Stories/BigCard.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+const hover = ref(false);
+function toggleHover() {
+  hover.value = true;
+}
+function leaveHover() {
+  hover.value = false;
+}
+
+const imageMobile = `/images/stories/mobile/${props.image}.jpg`;
+const imageDesktop = `/images/stories/desktop/${props.image}.jpg`;
+</script>
+
+<template>
+  <div
+    class="relative transition"
+    @mouseover="toggleHover"
+    @mouseleave="leaveHover"
+  >
+    <nuxt-link to="/">
+      <picture>
+        <source media="(max-width: 640px)" :srcset="imageMobile" />
+        <source media="(min-width: 1024px)" :srcset="imageDesktop" />
+        <img
+          :src="imageDesktop"
+          alt="Beautiful stories every time"
+          class="object-cover w-full h-full overflow-hidden"
+        />
+      </picture>
+      <footer
+        class="absolute bottom-0 left-0 flex flex-col justify-end pb-8 px-8 gap-1 text-white w-full h-[350px] bg-gradient-to-b from-white/0 to-black/75"
+      >
+        <div class="flex flex-col">
+          <span>{{ props?.date }}</span>
+          <h3 class="text-[24px] leading-[32px] font-semibold tracking-wide">
+            {{ props?.title }}
+          </h3>
+          <p class="mb-4">by {{ props?.author }}</p>
+          <div class="w-full border-t border-[#979797] pt-4">
+            <div class="flex justify-between">
+              <span class="uppercase text-sm">Read story</span>
+              <img
+                src="/images/shared/desktop/arrow-white.svg"
+                alt="arrow right"
+                class="max-h-[12px]"
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </nuxt-link>
+  </div>
+</template>
--- components/Stories/Overview.vue
+++ components/Stories/Overview.vue
@@ -1,15 +1,111 @@
+<script lang="ts" setup>
+const stories = [
+	{
+		title: 'The Mountains',
+		image: 'mountains',
+            date: 'April 16th 2020',
+            author: 'John Appleseed'
+	},
+	{
+		title: 'Sunset Cityscapes',
+		image: 'cityscapes',
+            date: 'April 14th 2020',
+            author: 'Benjamin Cruz'
+	},
+	{
+		title: '18 Days Voyage',
+		image: '18-days-voyage',
+            date: 'April 11th 2020',
+            author: 'Alexei Borodin'
+	},
+	{
+		title: 'Architecturals',
+		image: 'architecturals',
+            date: 'April 9th 2020',
+            author: 'Samantha Brooke'
+	},
+	{
+		title: 'World Tour 2019',
+		image: 'world-tour',
+            date: 'April 7th 2020',
+            author: 'Timothy Wagner'
+	},
+	{
+		title: 'Unforseen Corners',
+		image: 'unforeseen-corners',
+            date: 'April 3rd 2020',
+            author: 'William Malcolm'
+	},
+	{
+		title: 'King on Africa: Part ||',
+		image: 'king-on-africa',
+            date: 'March 29th 2020',
+            author: 'Tim Hillenburg'
+	},
+	{
+		title: 'The Trip To Nowhere',
+		image: 'trip-to-nowhere',
+            date: 'March 21st 2020',
+            author: 'Felicia Rourke'
+      },
+      {
+            title: 'Rage of The Sea',
+            image: 'rage-of-the-sea',
+            date: 'March 19th 2020',
+            author: 'Mohammed Abdul'
+      },
+      {
+            title: 'Running Free',
+            image: 'running-free',
+            date: 'March 16th 2020',
+            author: 'Michelle'
+      },
+      {
+            title: 'Behind the Waves',
+            image: 'behind-the-waves',
+            date: 'March 11th 2020',
+            author: 'Lamarr Wilson'
+      },
+      {
+            title: 'Calm Waters',
+            image: 'calm-waters',
+            date: 'March 9th 2020',
+            author: 'Samantha Brooke'
+      },
+      {
+            title: 'The Milky Way',
+            image: 'milky-way',
+            date: 'March 5th 2020',
+            author: 'Benjamin Cruz'
+      },
+      {
+            title: 'Night at The Dark Forest',
+            image: 'dark-forest',
+            date: 'March 4th 2020',
+            author: 'Mohammed Abdul'
+      },
+      {
+            title: 'Somwarpet’s Beauty',
+            image: 'somwarpet',
+            date: 'March 1st 2020',
+            author: 'Michelle'
+      },
+      {
+            title: 'Land of Dreams',
+            image: 'land-of-dreams',
+            date: 'February 25th 2020',
+            author: 'William Malcolm'
+      }
+]
+</script>
+
 <template>
 	<section>
+            <StoriesBigCard class="col-span-4" title="Hazy Full Moon Of Appalachia" image="moon-of-appalacia" date="March 2nd 2020" author="John Applessed" />
 		<div class="mx-auto max-w-screen-2xl sm:grid sm:grid-cols-2 md:grid-cols-4">
-            <StoriesCard title="The Mountains" image="mountains" date="April 16th 2020" author="John Appleseed" />
-            <StoriesCard title="Sunset Cityscapes" image="cityscapes" date="April 14th 2020" author="Benjamin Cruz" />
-            <StoriesCard title="18 Days Voyage" image="18-days-voyage" date="April 11th 2020" author="Alexei Borodin" />
-            <StoriesCard title="Architecturals" image="architecturals" date="April 9th 2020" author="Samantha Brooke" />
-
-            <StoriesCard title="World Tour 2019" image="world-tour" date="April 7th 2020" author="Timothy Wagner" />
-            <StoriesCard title="Unforseen Corners" image="unforeseen-corners" date="April 3rd 2020" author="Wiliam Malcolm" />
-            <StoriesCard title="King on Africa: Part ||" image="king-on-africa" date="March 29th 2020" author="Tim Hilenburg" />
-            <StoriesCard title="The Trip To Nowhere" image="trip-to-nowhere" date="March 21st 2020" author="Felicia Rourke" />
+			<template v-for="story in stories">
+				<StoriesCard :title="story.title" :image="story.image" :date="story.date" :author="story.author" />
+			</template>
 		</div>
 	</section>
 </template>
