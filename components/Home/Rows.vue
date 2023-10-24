aabd2cf90a27878df120f784bc006d114decb58d
Dev By RayRay <raymon@byrayray.dev>
2023-10-24 15:02:21 +0000

    Add max with for stories and rows for big screens

--- components/Home/Rows.vue
+++ components/Home/Rows.vue
@@ -1,88 +1,124 @@
 <template>
-	<GridTextLeft classLeft="bg-black text-white relative" mobileReverse>
-		<template v-slot:left>
-			<h2
-				class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
-			>
-				Create and share your photo stories.
-			</h2>
-			<p class="opacity-60 mb-6">
-				Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell
-				stories and connect with others.
-			</p>
-			<nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
-				><span>Get an invite</span> <img src="/images/shared/desktop/arrow-white.svg" class="max-h-[12px]"
-			/></nuxt-link>
-			<TheGradient vertical class="absolute left-0" />
-		</template>
-		<template v-slot:right>
-			<picture>
-				<source media="(max-width: 639px)" srcset="/images/home/mobile/create-and-share.jpg" />
-				<source media="(max-width: 1023px)" srcset="/images/home/tablet/create-and-share.jpg" />
-				<source media="(min-width: 1024px)" srcset="/images/home/desktop/create-and-share.jpg" />
-				<img
-					src="/images/home/desktop/create-and-share.jpg"
-					alt="Create and share your photo stories."
-					class="object-cover w-full h-full"
-				/>
-			</picture>
-		</template>
-	</GridTextLeft>
-	<GridTextRight>
-		<template v-slot:left>
-			<picture>
-				<source media="(max-width: 639px)" srcset="/images/home/mobile/beautiful-stories.jpg" />
-				<source media="(max-width: 1023px)" srcset="/images/home/tablet/beautiful-stories.jpg" />
-				<source media="(min-width: 1024px)" srcset="/images/home/desktop/beautiful-stories.jpg" />
-				<img
-					src="/images/home/desktop/beautiful-stories.jpg"
-					alt="Beautiful stories every time"
-					class="object-cover w-full h-full overflow-hidden"
-				/>
-			</picture>
-		</template>
-		<template v-slot:right>
-			<h2
-				class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
-			>
-				Beautiful stories <br />
-				every time
-			</h2>
-			<p class="opacity-60 mb-6">
-				We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps
-				and media from other networks. Then share your story with everyone.
-			</p>
-			<nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
-				><span>View the stories</span> <img src="/images/shared/desktop/arrow.svg" class="max-h-[12px]"
-			/></nuxt-link>
-		</template>
-	</GridTextRight>
-	<GridTextLeft mobileReverse>
-		<template v-slot:left>
-			<h2
-				class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
-			>
-				designed for everyone
-			</h2>
-			<p class="opacity-60 mb-6">
-				Photosnap can help you create stories that resonate with your audience. Our tool is designed for
-				photographers of all levels, brands, businesses you name it.
-			</p>
-			<nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
-				><span>View the stories</span> <img src="/images/shared/desktop/arrow.svg" class="max-h-[12px]"
-			/></nuxt-link>
-		</template>
-		<template v-slot:right>
-			<picture>
-				<source media="(max-width: 639px)" srcset="/images/home/mobile/designed-for-everyone.jpg" />
-				<source media="(max-width: 1023px)" srcset="/images/home/tablet/designed-for-everyone.jpg" />
-				<source media="(min-width: 1024px)" srcset="/images/home/desktop/designed-for-everyone.jpg" />
-				<img
-					src="/images/home/desktop/designed-for-everyone.jpg"
-					alt="Designed for everyone"
-					class="object-cover w-full h-full"
-				/>
-			</picture>
-		</template>
-	</GridTextLeft>
+  <div class="mx-auto max-w-screen-2xl">
+    <GridTextLeft classLeft="bg-black text-white relative" mobileReverse>
+      <template v-slot:left>
+        <h2
+          class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
+        >
+          Create and share your photo stories.
+        </h2>
+        <p class="opacity-60 mb-6">
+          Photosnap is a platform for photographers and visual storytellers. We
+          make it easy to share photos, tell stories and connect with others.
+        </p>
+        <nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
+          ><span>Get an invite</span>
+          <img
+            src="/images/shared/desktop/arrow-white.svg"
+            class="max-h-[12px]"
+        /></nuxt-link>
+        <TheGradient vertical class="absolute left-0" />
+      </template>
+      <template v-slot:right>
+        <picture>
+          <source
+            media="(max-width: 639px)"
+            srcset="/images/home/mobile/create-and-share.jpg"
+          />
+          <source
+            media="(max-width: 1023px)"
+            srcset="/images/home/tablet/create-and-share.jpg"
+          />
+          <source
+            media="(min-width: 1024px)"
+            srcset="/images/home/desktop/create-and-share.jpg"
+          />
+          <img
+            src="/images/home/desktop/create-and-share.jpg"
+            alt="Create and share your photo stories."
+            class="object-cover w-full h-full"
+          />
+        </picture>
+      </template>
+    </GridTextLeft>
+    <GridTextRight>
+      <template v-slot:left>
+        <picture>
+          <source
+            media="(max-width: 639px)"
+            srcset="/images/home/mobile/beautiful-stories.jpg"
+          />
+          <source
+            media="(max-width: 1023px)"
+            srcset="/images/home/tablet/beautiful-stories.jpg"
+          />
+          <source
+            media="(min-width: 1024px)"
+            srcset="/images/home/desktop/beautiful-stories.jpg"
+          />
+          <img
+            src="/images/home/desktop/beautiful-stories.jpg"
+            alt="Beautiful stories every time"
+            class="object-cover w-full h-full overflow-hidden"
+          />
+        </picture>
+      </template>
+      <template v-slot:right>
+        <h2
+          class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
+        >
+          Beautiful stories <br />
+          every time
+        </h2>
+        <p class="opacity-60 mb-6">
+          We provide design templates to ensure your stories look terrific.
+          Easily add photos, text, embed maps and media from other networks.
+          Then share your story with everyone.
+        </p>
+        <nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
+          ><span>View the stories</span>
+          <img src="/images/shared/desktop/arrow.svg" class="max-h-[12px]"
+        /></nuxt-link>
+      </template>
+    </GridTextRight>
+    <GridTextLeft mobileReverse>
+      <template v-slot:left>
+        <h2
+          class="text-[32px] leading-[40px] lg:text-[40px] lg:leading-[48px] uppercase font-semibold tracking-wide"
+        >
+          designed for everyone
+        </h2>
+        <p class="opacity-60 mb-6">
+          Photosnap can help you create stories that resonate with your
+          audience. Our tool is designed for photographers of all levels,
+          brands, businesses you name it.
+        </p>
+        <nuxt-link to="/" class="text-sm uppercase flex gap-4 items-center"
+          ><span>View the stories</span>
+          <img src="/images/shared/desktop/arrow.svg" class="max-h-[12px]"
+        /></nuxt-link>
+      </template>
+      <template v-slot:right>
+        <picture>
+          <source
+            media="(max-width: 639px)"
+            srcset="/images/home/mobile/designed-for-everyone.jpg"
+          />
+          <source
+            media="(max-width: 1023px)"
+            srcset="/images/home/tablet/designed-for-everyone.jpg"
+          />
+          <source
+            media="(min-width: 1024px)"
+            srcset="/images/home/desktop/designed-for-everyone.jpg"
+          />
+          <img
+            src="/images/home/desktop/designed-for-everyone.jpg"
+            alt="Designed for everyone"
+            class="object-cover w-full h-full"
+          />
+        </picture>
+      </template>
+    </GridTextLeft>
+  </div>
 </template>
--- components/Home/Stories.vue
+++ components/Home/Stories.vue
@@ -6,7 +6,7 @@ import { StoriesCard } from '#build/components';
 import { StoriesCard } from '#build/components';
 <template>
 	<section>
-		<div class="mx-auto max-w-8xl md:grid md:grid-cols-2 lg:grid-cols-4">
+		<div class="mx-auto max-w-screen-2xl md:grid md:grid-cols-2 lg:grid-cols-4">
             <StoriesCard title="The Mountains" image="mountains" date="April 16th 2020" author="John Appleseed" />
             <StoriesCard title="Sunset Cityscapes" image="cityscapes" date="April 14th 2020" author="Benjamin Cruz" />
             <StoriesCard title="18 Days Voyage" image="18-days-voyage" date="April 11th 2020" author="Alexei Borodin" />
